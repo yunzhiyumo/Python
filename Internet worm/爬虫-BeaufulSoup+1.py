be3614d8031e0cb2dc23391e8b01ad565bfc8025
Richard Liu <pm123456@aliyun.com>
2018-01-12 23:06:46 +0800

    Update 爬虫-BeaufulSoup+1.py

--- Internet worm/爬虫-BeaufulSoup+1.py
+++ Internet worm/爬虫-BeaufulSoup+1.py
@@ -3,12 +3,11 @@
 
 # In[82]:
 
-
-from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 h = urlopen('http://www.woshipm.com/pd/900844.html').read().decode('utf-8')
-print('h')
+print(h)
 
 soup = BeautifulSoup(h,features='lxml')
 a_targ = soup.find_all('a')
